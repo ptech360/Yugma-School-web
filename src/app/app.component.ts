ef7a70dd4294828fed771255295be6bceeca4073
TusharBalar <balar.tushar1@gmail.com>
2017-01-12 14:42:36 +0530

    Added sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
+
+declare var $;
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
+  styles: [`
+    .tb-grey {
+      background-color: #424242 !important;
+    }
+    `]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('selectElem') el:ElementRef;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    $(this.el.nativeElement).sideNav({draggable: true,closeOnClick: true});
+  }
+
+
 }
