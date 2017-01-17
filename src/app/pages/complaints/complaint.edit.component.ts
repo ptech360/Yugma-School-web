995f98c23f5d3c9c912b4154383287b06e838a19
TusharBalar <balar.tushar1@gmail.com>
2017-01-17 17:06:20 +0530

    Add more content to edit complaint

--- src/app/pages/complaints/complaint.edit.component.ts
+++ src/app/pages/complaints/complaint.edit.component.ts
@@ -12,8 +12,28 @@ declare let Materialize;
   selector: 'edit-complaint',
   styleUrls: [],
   template: `
-  <button (click)="goBack()" class="btn">Back</button>
-    <h3>Edit</h3>
+    <button (click)="goBack()" class="btn">Back</button>
+    <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Assigned To</label>
+        </div>
+      </div>
+      <div class="row">
+        <p>
+          <input class="with-gap" name="group1" type="radio" id="test1" checked />
+          <label for="test1">Red</label>
+          <input class="with-gap" name="group1" type="radio" id="test2" checked />
+          <label for="test2">blue</label>
+          <input class="with-gap" name="group1" type="radio" id="test3" checked />
+          <label for="test3">green</label>
+        </p>
+      </div>
+    </form>
+  </div>
   `
 })
 
@@ -25,7 +45,7 @@ export class EditComplaint {
   }
 
   goBack() {
-    this.location.back();
+    window.history.back();
   }
 
 }
